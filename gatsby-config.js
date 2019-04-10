2fd0aff98a3fd8f0ce286ba3b8db62839533183d
Klemoh <clement_alesio@yahoo.com>
2019-04-10 14:11:12 +0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lets Gym`,
+    description: `This is an online platform for gym lovers to buy their gym products,weights,accessories etc`,
+    author: `@AlessionTech`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
